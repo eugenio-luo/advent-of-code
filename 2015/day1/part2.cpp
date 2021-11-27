b3ece755c37633edd03f16d925752a0a7ee66225
eugenio <eugenioluo@gmail.com>
2021-11-27 20:14:26 +0100

    added part 2

---
+++ 2015/day1/part2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+
+int solve(const std::string& input) {
+    int floor = 0;
+    int pos = 0;
+    
+    for (char letter : input) {
+        ++pos;
+        floor += (letter == '(') ? 1 : -1;
+
+        if (floor < 0) {
+            return pos;
+        }
+    }
+    
+    return -1;
+}
+
+int main() {
+    std::ifstream t("input");
+    if (t.fail()) {
+        std::cout << "ERROR: " << strerror(errno) << '\n';
+    } else {
+        std::stringstream buffer;
+        buffer << t.rdbuf();
+
+        std::cout << solve(buffer.str()) << '\n';     
+    }
+}
