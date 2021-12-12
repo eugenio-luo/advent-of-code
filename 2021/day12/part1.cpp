c5a4d7081ff812706b884e4508d87ae08e4c78d2
eugenio <eugenioluo@gmail.com>
2021-12-12 16:55:18 +0100

    day12 added

---
+++ 2021/day12/part1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <unordered_map>
+#include <vector>
+#include <cctype>
+#include <algorithm>
+
+struct Cave {
+    std::string name;
+    bool big;
+    int visited;
+    std::vector<Cave*> nodes;
+};
+
+typedef std::unordered_map<std::string, Cave> CaveMap;
+
+bool allUppers(std::string& s) {
+    return std::all_of(std::begin(s), std::end(s), [](unsigned char c){ return std::isupper(c); });
+}
+
+int path(CaveMap& caves, const std::string& name, bool twice = false) {
+    auto& actualCave = caves[name];
+    int paths{};
+    ++actualCave.visited;
+
+    for (auto& node : actualCave.nodes) {
+
+        if (node->name == "end") {
+            ++paths;
+        } else if (node->big || node->visited < 1) {
+            paths += path(caves, node->name, twice);
+        }
+    }
+    
+    --actualCave.visited;
+    return paths;
+}
+
+
+int solve(std::istream& input) {
+    CaveMap caves{};
+    std::string line;
+    while (std::getline(input, line)) {
+        auto found = line.find('-');
+        auto firstName = line.substr(0, found);
+        auto secondName = line.substr(found + 1);
+
+        auto& firstCave = caves[firstName];
+        auto& secondCave = caves[secondName];
+
+        firstCave.name = firstName;
+        secondCave.name = secondName;
+        firstCave.big = allUppers(firstName);
+        secondCave.big = allUppers(secondName);
+        if (secondName != "start") firstCave.nodes.push_back(&secondCave);
+        if (firstName != "start") secondCave.nodes.push_back(&firstCave);
+    }
+    
+    return path(caves, "start");
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "[ERROR] missing argument [input]\n";
+    } else {    
+        std::ifstream input(argv[1]);
+        if (input.fail()) std::cerr << "[ERROR] " << strerror(errno) << '\n';
+
+        std::cout << solve(input) << '\n';
+    }
+}
--- timing.cpp
+++ timing.cpp
@@ -10,8 +10,9 @@ int main(int argc, char* argv[]) {
     } else {
         std::vector<long double> timings;
 
+        std::system("rm results");
         double progress{};
-        double progressStep = (1.0 / (std::stod(argv[2]) - 1)) + 0.001;
+        double progressStep = (1.0 / (std::stod(argv[2]) - 1)) + (0.01 / std::stod(argv[2]));
         int barWidth = 70;
         std::cout << "\nCOMPILING AND EXECUTING\n";
         for (int i{1}; i <= std::stoi(argv[2]); ++i) {
@@ -19,11 +20,11 @@ int main(int argc, char* argv[]) {
                 std::stringstream com;
                 com << "g++ " << argv[1] << "/day";
                 if (i < 10) com << "0";
-                com << i << "/part" << j << ".cpp -o a.out";
+                com << i << "/part" << j << ".cpp -o ccc.out";
                 std::system(com.str().c_str());
 
                 com.str("");
-                com << "./a.out " << argv[1] << "/day";
+                com << "./ccc.out " << argv[1] << "/day";
                 if (i < 10) com << "0";
                 com << i << "/input >> results";
                 auto start = std::chrono::high_resolution_clock::now();
@@ -53,16 +54,16 @@ int main(int argc, char* argv[]) {
         int day{1};
         int part{1};
         long double total{};
-        std::cout << "year\tday\tpart\ts\n";
-        for (auto ms : timings) {
-            std::cout << argv[1] << '\t' << day << '\t' << part << "\t\033[1;32m" << ms << "\n\033[0m";
+        std::cout << "year\tday\tpart\tseconds\n";
+        for (auto s : timings) {
+            std::cout << argv[1] << '\t' << day << '\t' << part << "\t\033[1;32m" << s << "\n\033[0m";
             ++part;
             if (part == 3) {
                 ++day;
                 part = 1;
             }
 
-            total+= ms;
+            total+= s;
         }
         std::cout << "\n\033[1;31mTOTAL:\t\t\t" << total << "\n\n";
     }
